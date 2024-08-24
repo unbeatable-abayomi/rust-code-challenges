1ff0fa89e18a3d925af18914ac4c84e0a098df39
unbeatable-abayomi <igwubor@outlook.com>
2024-08-25 00:40:57 +0100

    Completed Challenge Two

--- challenge_two/src/main.rs
+++ challenge_two/src/main.rs
@@ -5,6 +5,31 @@ fn unique(mut a: Vec<i32>) -> Vec<i32> {
   a
 }
 
+
+// advanced 1: use generic types
+// fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort();
+//     a.dedup();
+//     a
+// }
+
+
+
+// // advanced 2: keep items in order
+// fn unique(a: Iterable<T>) -> Vec<T> {
+// todo!();
+// }
+
+
+// advanced 3: use iterators
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//  todo!();
+// }
+
+
+
+
+
 fn main() {
     let input: Vec<i32> = vec![2,1,1];
     let answer: Vec<i32> = unique(input);
@@ -15,6 +40,7 @@ fn main() {
 
 #[test]
 fn empty_list() {
+  //  let input: Vec<i64> = vec![];
     let input = vec![];
     let expected_output = vec![];
     let actual_output = unique(input);
