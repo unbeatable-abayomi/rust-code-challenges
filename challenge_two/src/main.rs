15b4a91b0efdffc2e455afd341ab6c2c66f9038d
unbeatable-abayomi <igwubor@outlook.com>
2024-08-25 00:24:54 +0100

    WIP Completed Challenge Two

--- challenge_two/src/main.rs
+++ challenge_two/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+
+fn unique(mut a: Vec<i32>) -> Vec<i32> {
+  a.sort();
+  a.dedup();
+  a
 }
+
+fn main() {
+    let input: Vec<i32> = vec![2,1,1];
+    let answer: Vec<i32> = unique(input);
+    println!("unique items -> {:?}", answer);
+}
+
+
+
+#[test]
+fn empty_list() {
+    let input = vec![];
+    let expected_output = vec![];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list() {
+    let input = vec![1, 4, 5];
+    let expected_output = vec![1, 4, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1, 5, 2];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![1, 5, 2, 2, 1];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let mut input = vec![1, 5, 2, 2, 1];
+    input.sort_by(|x,y| x.partial_cmp(y).unwrap());
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
---
+++ challenge_two/target/debug/deps/challenge_two-7a1c0a19ed14a3c3.d
@@ -0,0 +1,5 @@
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_two/target/debug/deps/challenge_two-7a1c0a19ed14a3c3: src/main.rs
+
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_two/target/debug/deps/challenge_two-7a1c0a19ed14a3c3.d: src/main.rs
+
+src/main.rs:
