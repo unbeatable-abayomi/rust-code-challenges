d84fa673a25902847d262108ca94608ae1a6c41e
unbeatable-abayomi <igwubor@outlook.com>
2024-09-01 10:31:23 +0100

    complete challange four

--- challenge_four/src/main.rs
+++ challenge_four/src/main.rs
@@ -1,3 +1,22 @@
+//Case Sensetive Sort.
+
+fn sort_usernames<T: AsRef<str> + Ord>(usernames: &mut Vec<T>){
+   usernames
+   .sort_by_cached_key(|x: &T| x.as_ref().to_lowercase());
+}
+
+
 fn main() {
-    println!("Hello, world!");
+   let mut users : Vec<&str> = vec!["Todd","Amy","mike99","Jennifer","alison"];
+   println!("unsorted: {:?}", &users);
+   sort_usernames(&mut users);
+   println!("sorted: {:?}", &users);
+}
+
+#[test]
+fn five_users(){
+    let mut users : Vec<&str> = vec!["Todd","Amy","mike99","Jennifer","alison"];
+    let sorted : Vec<&str> = vec!["alison","Amy","Jennifer","mike99","Todd"];
+    sort_usernames(&mut users);
+    assert_eq!(users, sorted);
 }
---
+++ challenge_four/target/debug/deps/challenge_four-b09dc8797c9c2900.d
@@ -0,0 +1,5 @@
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_four/target/debug/deps/challenge_four-b09dc8797c9c2900: src/main.rs
+
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_four/target/debug/deps/challenge_four-b09dc8797c9c2900.d: src/main.rs
+
+src/main.rs:
