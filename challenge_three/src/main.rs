20d20f4abf63ff824980b67ec6526a8c6871c49a
unbeatable-abayomi <igwubor@outlook.com>
2024-09-01 09:58:58 +0100

    complete challange three

--- challenge_three/src/main.rs
+++ challenge_three/src/main.rs
@@ -1,15 +1,22 @@
-fn info (a: &String) {
+fn info <T: std::fmt::Display>(a: &T) {
     println!("{}", a);
 }
+
+// fn info(a: &str) {
+//     println!("{}", a);
+// }
 
 
 
 
 fn main() {
-   // let a : &str = "?";
-    let b : String = "Hello,String".to_string();
+   let a : &str = "?";
+    //let b : &String = &"Hello,String".to_string();
+    //let input : u32 = 200;
+    //let vectord: String = "Hello";
     //info(&a);
-    info(&b);
+    info(&a);
+    //info2(&vectord);
 
 }
 
@@ -24,3 +31,31 @@ fn string(){
     let input = String::from("Rust");
     info(&input);
 }
+
+
+#[test]
+fn u32dd(){
+    let input : u32 = 200;
+    info(&input);
+}
+
+
+// #[test]
+// fn chars() {
+//     let input = 'r';
+//     info(&input);
+// }
+
+// #[test]
+// fn cstring() {
+//     use std::ffi::{CString};
+//     let input = CString::new("Rust").unwrap();
+//     info(&input);
+// }
+
+// #[test]
+// fn path() {
+//     use std::path::Path;
+//     let input = Path::new("/tmp/rust");
+//     info(input);
+// }
---
+++ challenge_three/target/debug/deps/challenge_three-b5ab1124518b093d.d
@@ -0,0 +1,5 @@
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_three/target/debug/deps/challenge_three-b5ab1124518b093d: src/main.rs
+
+/home/abayomi/Projects/rust/rust_code_challenges/challenge_three/target/debug/deps/challenge_three-b5ab1124518b093d.d: src/main.rs
+
+src/main.rs:
