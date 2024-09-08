8144353270ee108851d5e667e9ac8bf551914a41
unbeatable-abayomi <igwubor@outlook.com>
2024-09-08 12:53:26 +0100

    added Mutable Ref

--- playground_project/src/main.rs
+++ playground_project/src/main.rs
@@ -33,4 +33,36 @@ fn main() {
     }
 
     println!("outer number: {}", outer_number);
+
+    let rocket_fuel = 1;
+    let mut rocket_fuel_string = String::from("Rocket fuel");
+   process_fuel(rocket_fuel,&mut rocket_fuel_string); // This is a copy because we're passing a copy of the value,Datatype here is also an interger an it's strored on the stack, which has an implicity copy 
+
+    println!("Rocket fuel: {}, Rocket fuel String {}", rocket_fuel, rocket_fuel_string);
 }
+
+
+fn process_fuel(mut propellent:i32, roc_string: &mut String)-> usize{
+    propellent += 10;
+    roc_string.push_str("Rust");
+    println!("Propellent Integer: {} Propellent String: {} ", propellent, roc_string);
+    roc_string.len()
+}
+
+//Restrictions
+
+//When using a mutable refrence, you cannnot create other refrences
+//Prevent Data Races
+
+//NOTE : PLs
+
+//You can only create one mutable refrence at a time
+//Eg
+// let ref1 = &mut var;
+//You can create multiple immutable refrences.
+//Eg
+// let ref1 = & var;
+// let ref2 = & var;
+
+//NOTE: Pls ==> 
+//You can't create a mutable refrence and any other references.
